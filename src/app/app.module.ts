5da8c3dbd36e4bae2da42b09421941fc553b52ef
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-10-30 16:07:23 +0100

    svg finally works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import {
   MatInputModule,
@@ -13,13 +14,14 @@ import {
   MatExpansionModule,
   MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
-import { TicketCreateComponent } from "./tickets/ticket-create/ticket-create.component";
-import { HeaderComponent } from "./header/header.component";
-import { TicketListComponent } from "./tickets/ticket-list/ticket-list.component";
+import { TicketCreateComponent } from './tickets/ticket-create/ticket-create.component';
+import { HeaderComponent } from './header/header.component';
+import { TicketListComponent } from './tickets/ticket-list/ticket-list.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { BaseComponent } from './base/base.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 const material = [
   MatInputModule,
@@ -45,6 +47,8 @@ const material = [
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    AngularSvgIconModule,
     RouterModule.forRoot([{
       path: '',
       component: BaseComponent
